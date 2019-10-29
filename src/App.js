c42f426f07baaa14d3ab895841ae904aa291331c
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-10-29 15:43:18 -0600

    Header and Meme container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
 }
 
 export default App;
